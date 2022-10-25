d193797ce2f7a925f3a6417f9c7ac4cef9595782
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-10-25 18:23:14 +0600

    using loop in a template

--- src/pages/views.py
+++ src/pages/views.py
@@ -13,5 +13,13 @@ def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {});
 
 def about_view(request, *args, **kwargs):
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [123, 4242, 12313, 'abc']
+    }
     # return HttpResponse("<h1>About Page</h1>");
-    return render(request, "about.html", {});
+    return render(request, "about.html", my_context);
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Social Page</h1>");
